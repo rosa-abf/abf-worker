4926b2cdd7b8df2d39d2284cee6d7f66709131cd
Vokhmin Alexey V <avokhmin@gmail.com>
2012-10-24 18:49:09 +0400

    fixed cleaning worker

--- lib/abf-worker/worker.rb
+++ lib/abf-worker/worker.rb
@@ -4,26 +4,28 @@ module AbfWorker
     @queue = :worker
 
     def self.perform(os, arch, script_path)
-      vm_name = "#{os}_#{arch}_#{Process.getpgid(Process.ppid())}"
+      vm_name = "#{os}_#{arch}_#{get_worker_id}"
       find_or_create_vagrant_file vm_name, os, arch
       env = Vagrant::Environment.new(:vagrantfile_name => "vagrantfiles/#{vm_name}")
 
-      puts 'Enter sandbox mode'
-      env.cli 'sandbox', 'on', vm_name
+#      puts 'Enter sandbox mode'
+#      env.cli 'sandbox', 'on', vm_name
 
       puts 'Start to run vagrant-up...'
       env.cli 'up', vm_name
       puts 'Finished running vagrant-up'
 
-      puts 'Rollback actions'
-      env.cli 'sandbox', 'rollback', vm_name
+#      puts 'Rollback actions'
+#      env.cli 'sandbox', 'rollback', vm_name
 
-      puts 'Exit sandbox mode'
-      env.cli 'sandbox', 'off', vm_name      
+#      puts 'Exit sandbox mode'
+#      env.cli 'sandbox', 'off', vm_name      
 
       puts 'Start to run vagrant-halt...'
       env.cli 'halt', vm_name
       puts 'Finished running vagrant-halt'
+    rescue Resque::TermException
+      clean
     end
 
     def self.find_or_create_vagrant_file(vm_name, os, arch)
@@ -46,6 +48,31 @@ module AbfWorker
           file.close unless file.nil?
         end
       end
+    end
+
+    def self.clean
+      files = []
+      worker_id = get_worker_id
+      path = Dir.pwd.to_s << '/vagrantfiles'
+      Dir.new(path).entries.each do |n|
+        if File.file?(path + "/#{n}") && n =~ /#{worker_id}/
+          files << n
+        end
+      end
+      puts files.inspect
+      files.each do |f|
+        env = Vagrant::Environment.new(:vagrantfile_name => "vagrantfiles/#{f}")
+        puts 'Halt VM...'
+        env.cli 'halt', '-f'
+        puts 'Destroy VM...'
+        env.cli 'destroy', '-f'
+
+        File.delete(path + "/" + f)
+      end
+    end
+
+    def self.get_worker_id
+      Process.getpgid(Process.ppid())
     end
 
   end
