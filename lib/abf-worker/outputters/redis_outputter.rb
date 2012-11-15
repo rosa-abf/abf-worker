c36f7f888c334ed4c8e84089635fc0b536e07878
Vokhmin Alexey V <avokhmin@gmail.com>
2012-11-15 06:19:52 +0400

    fix outputter

--- lib/abf-worker/outputters/redis_outputter.rb
+++ lib/abf-worker/outputters/redis_outputter.rb
@@ -21,7 +21,7 @@ module AbfWorker
         line = data.to_s.gsub(/^.*\:{1}/, '')
         unless line.empty?
           last_line = @buffer.last
-          if last_line && (line =~ /\#/) && (line[-1, 1] == last_line[-1, 1])
+          if last_line && (line.strip =~ /^[\#]+$/) && (line[-1, 1] == last_line[-1, 1])
             last_line << line.strip
           else
             l = @line_number.to_s
