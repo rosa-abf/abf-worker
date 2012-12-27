e14ec5bbbe856c24926d74319156a8ee77e6c274
Vokhmin Alexey V <avokhmin@gmail.com>
2012-12-27 19:04:20 +0400

    use VmInspector at starting VM at first time

--- lib/abf-worker/runners/vm.rb
+++ lib/abf-worker/runners/vm.rb
@@ -82,7 +82,9 @@ module AbfWorker
           sleep 30
           logger.info "==> [#{Time.now.utc}] Configure VM..."
           # Halt, because: The machine 'abf-worker_...' is already locked for a session (or being unlocked)
-          @vagrant_env.cli 'halt', @vm_name
+          run_with_vm_inspector {
+            @vagrant_env.cli 'halt', @vm_name
+          }
           sleep 20
           vm_id = get_vm.id
           # see: #initialize_vagrant_env: 37
@@ -93,7 +95,9 @@ module AbfWorker
           end
 
           sleep 10
-          @vagrant_env.cli 'up', @vm_name
+          run_with_vm_inspector {
+            @vagrant_env.cli 'up', @vm_name
+          }
           sleep 30
           if @os == 'mdv'
             execute_command('urpmi.update -a', {:sudo => true})
