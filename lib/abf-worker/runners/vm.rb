cf3afa93e18255322f5a608348b1775ffe6aba43
Vokhmin Alexey V <avokhmin@gmail.com>
2013-01-15 03:41:09 +0400

    hot fix

--- lib/abf-worker/runners/vm.rb
+++ lib/abf-worker/runners/vm.rb
@@ -62,7 +62,7 @@ module AbfWorker
           :cwd => vagrantfiles_folder,
           :vagrantfile_name => @vm_name
         )
-        system "sudo chown -R rosa:rosa #{@share_folder}" if update_share_folder
+        system "sudo chown -R rosa:rosa #{@share_folder}/../" if update_share_folder
         # Hook for fix:
         # ERROR warden: Error occurred: uninitialized constant VagrantPlugins::ProviderVirtualBox::Action::Customize::Errors
         # on vm_config.vm.customizations << ['modifyvm', :id, '--memory',  '#{memory}']
