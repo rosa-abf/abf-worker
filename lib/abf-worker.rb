92ce55734c9469409ce517e751130d6d7c5e734b
Vokhmin Alexey V <avokhmin@gmail.com>
2013-01-16 16:52:06 +0400

    #20: add airbrake

--- lib/abf-worker.rb
+++ lib/abf-worker.rb
@@ -1,4 +1,5 @@
 require 'resque'
+require 'airbrake'
 
 
 root = File.dirname(__FILE__) + '/..'
@@ -8,6 +9,10 @@ resque_config = YAML.load_file("#{root}/config/resque.yml")
 Resque.redis = resque_config[env]
 
 APP_CONFIG = YAML.load_file("#{root}/config/application.yml")[env]
+
+Airbrake.configure do |config|
+  config.api_key = APP_CONFIG['airbrake_api_key']
+end
 
 require 'abf-worker/base_worker'
 require 'abf-worker/iso_worker'
