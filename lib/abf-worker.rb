5ac05d9fd13ed57d16e4ee553ac66edee398ac49
Vokhmin Alexey V <avokhmin@gmail.com>
2013-06-20 17:04:29 +0400

    #13: removed unnecessary code

--- lib/abf-worker.rb
+++ lib/abf-worker.rb
@@ -29,21 +29,6 @@ Airbrake.configure do |config|
   config.api_key = APP_CONFIG['airbrake_api_key']
 end
 
-# Resque.before_first_fork do
-#   NewRelic::Agent.manual_start(dispatcher:              :resque,
-#                                sync_startup:            true,
-#                                start_channel_listener:  true,
-#                                report_instance_busy:    false)
-# end
-
-# Resque.before_fork do |job|
-#   NewRelic::Agent.register_report_channel(job.object_id)
-# end
-
-# Resque.after_fork do |job|
-#   NewRelic::Agent.after_fork(report_to_channel: job.object_id)
-# end
-
 require 'abf-worker/base_worker'
 require 'abf-worker/iso_worker'
 require 'abf-worker/rpm_worker'
